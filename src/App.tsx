e2ef5b9547a5e3959bf91020b31dddfa0df3f5c7
gunyongbok <qhrrjsdyd123@gmail.com>
2025-01-18 23:33:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const App = () => {
+  return <div>{/* <Outlet/> */}</div>;
+};
+
+export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // 레이아웃 컴포넌트
+    children: [
+      {
+        path: "",
+        //   element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
