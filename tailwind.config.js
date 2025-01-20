19130cea7296d30ceeeaac6ecfac1c6e06823304
gunyongbok <qhrrjsdyd123@gmail.com>
2025-01-20 17:25:11 +0900

    feat: 홈페이지 소개 및 헤더 상단 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
       spacing: {
         "50px": "50px",
       },
+      fontFamily: {
+        pretendard: ["Pretendard", "sans-serif"], // Pretendard 추가
+      },
+      fontSize: {
+        32: "32px",
+        28: "28px",
+        20: "20px",
+        17: "17px",
+        16: "16px",
+        14: "14px",
+        12: "12px",
+        11: "11px",
+      },
     },
   },
   plugins: [],
