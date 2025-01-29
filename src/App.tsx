1968089f4cf85bb8ebcfc3019143d613d5265b8a
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-01-30 01:48:43 +0900

    feat: SearchBar 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import LoginModal from './components/Login/LoginModal';
 import { useLoginMoalStore } from './store/loginStore';
+import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
   const { openLoginModal } = useLoginMoalStore();
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center max-h-screen">
       {/* 로그인 모달창 오픈 예시 버튼 */}
       <button onClick={openLoginModal} className="btn">
         로그인 창 열기
       </button>
+      <SearchBar />
       <Outlet />
       <LoginModal />
     </div>
---
+++ src/services/schemas/searchSchema.ts
@@ -0,0 +1,10 @@
+import z from 'zod';
+
+export const searchSchema = z.object({
+  title: z
+    .string()
+    .max(20, { message: '제목은 20글자를 넘을 수 없습니다.' })
+    .min(1, { message: '' }),
+});
+
+export type SearchForm = z.infer<typeof searchSchema>;
