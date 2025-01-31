dd444aac2ceaeb73546b8db634bb150944def3a3
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-01 01:01:07 +0900

    feat: 페이지네이션, 섹션헤더 컴포넌트 추가
     - 샤드cn의 페이지네이션 컴포넌트를 적절히 수정
     - 공통 컴포넌트인 섹션헤더 추가

---
+++ src/components/CustomPagination.tsx
@@ -0,0 +1,55 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import { useState } from 'react';
+
+interface CustomPaginationProps {
+  total: number;
+  current: number;
+}
+
+function CustomPagination({ total, current }: CustomPaginationProps) {
+  const [pageNums, setPageNums] = useState([1, 2, 3]);
+
+  const onPrevClick = () => {};
+
+  const onNextClick = () => {};
+  return (
+    <Pagination>
+      <PaginationContent>
+        {current >= 1 || current <= 3 ? (
+          <PaginationItem>
+            <PaginationPrevious onClick={onPrevClick} />
+          </PaginationItem>
+        ) : null}
+        {pageNums.map((num) => (
+          <PaginationItem>
+            <PaginationLink isActive={current === num}>{num}</PaginationLink>
+          </PaginationItem>
+        ))}
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="">{total}</PaginationLink>
+        </PaginationItem>
+        {current >= total - 3 && current <= total ? (
+          <PaginationItem>
+            <PaginationPrevious onClick={onPrevClick} />
+          </PaginationItem>
+        ) : null}
+        <PaginationItem>
+          <PaginationNext onClick={onNextClick} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
+
+export default CustomPagination;
