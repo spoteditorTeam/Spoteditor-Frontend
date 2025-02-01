f20caca2dba8dafba9518fc34345675e93d9fa9f
gunyongbok <qhrrjsdyd123@gmail.com>
2025-02-01 17:59:53 +0900

    feat: 홈페이지 sort by populariy 규격 구현 및 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,16 @@
 import MainFooter from "../../components/Footer/MainFooter";
 import MainHeader from "../../components/Header/MainHeader";
 import HomePageIntroContent from "../../feature/homepage/HomePageIntroContent";
+import SortByPopularity from "../../feature/homepage/SortByPopularity";
 import YouCanBeSpecialEditor from "../../feature/homepage/YouCanBeSpecialEditor";
 
 const HomePage = () => {
   return (
-    <div className="w-full h-screen bg-red-50">
+    <div className="w-full h-screen bg-white">
       <MainHeader />
       <HomePageIntroContent />
       <div className="flex flex-col w-full h-[2720px] mt-16 px-50px bg-white">
-        <div className="w-full h-[603px] bg-red-400 mb-20"></div>
+        <SortByPopularity />
         <YouCanBeSpecialEditor />
         <div className="w-full h-[1524px] bg-red-400 "></div>
       </div>
