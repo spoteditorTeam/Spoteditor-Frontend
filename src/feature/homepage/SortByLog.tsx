b0d7faa73ed37904aac26bd11debad6635f6d8b5
gunyongbok <qhrrjsdyd123@gmail.com>
2025-02-01 18:53:53 +0900

    feat: 홈페이지 sort by log 규격 구현 및 적용

---
+++ src/feature/homepage/SortByLog.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+const SortByLog = () => {
+  return (
+    <div className="w-full h-[1524px] ">
+      <div className="flex w-[156px] flex-col gap-4 mb-12">
+        <div className="font-untitled text-32 h-[23px] text-[#ABAFB5]">
+          Sort by
+        </div>
+        <div className="font-untitled text-32 h-[23px] text-[#242528]">Log</div>
+      </div>
+      <div className="w-full flex">
+        <div className="w-1/2  h-[1328px] flex flex-wrap">
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2  h-[1328px] flex flex-wrap">
+          <div className="w-[664px] mr-5 mb-6">
+            <div className="w-full h-[550px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+          <div className="w-[314px] mr-5">
+            <div className="w-full h-[218px] bg-red-100 mb-2"></div>
+            <div className="w-full font-pretendard text-16 font-bold text-black">
+              혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페
+            </div>
+            <div className="w-full font-pretendard text-17 font-normal text-[#ABAFB5]">
+              서울 | 위치 세부정보
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SortByLog;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import MainFooter from "../../components/Footer/MainFooter";
 import MainHeader from "../../components/Header/MainHeader";
 import HomePageIntroContent from "../../feature/homepage/HomePageIntroContent";
+import SortByLog from "../../feature/homepage/SortByLog";
 import SortByPopularity from "../../feature/homepage/SortByPopularity";
 import YouCanBeSpecialEditor from "../../feature/homepage/YouCanBeSpecialEditor";
 
@@ -12,7 +13,7 @@ const HomePage = () => {
       <div className="flex flex-col w-full h-[2720px] mt-16 px-50px bg-white">
         <SortByPopularity />
         <YouCanBeSpecialEditor />
-        <div className="w-full h-[1524px] bg-red-400 "></div>
+        <SortByLog />
       </div>
       <MainFooter />
     </div>
