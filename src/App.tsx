c75470dbd6021362a7608b66e0a0d125148e281d
gunyongbok <qhrrjsdyd123@gmail.com>
2025-02-02 21:11:58 +0900

    resolve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { Outlet } from 'react-router-dom';
-import LoginModal from './components/Login/LoginModal';
-import { useLoginMoalStore } from './store/loginStore';
-import SearchBar from './components/SearchBar/SearchBar';
+import { Outlet } from "react-router-dom";
+import MainHeader from "./components/Header/MainHeader";
 
 const App = () => {
-  const { openLoginModal } = useLoginMoalStore();
   return (
-    <div className="flex flex-col justify-center max-h-screen">
-      {/* 로그인 모달창 오픈 예시 버튼 */}
-      <button onClick={openLoginModal} className="btn">
-        로그인 창 열기
-      </button>
-      <SearchBar />
+    <div className="flex flex-col items-center web:w-[1440px] web:mx-auto mobile:max-w-[768px]">
+      <MainHeader />
       <Outlet />
-      <LoginModal />
     </div>
   );
 };
