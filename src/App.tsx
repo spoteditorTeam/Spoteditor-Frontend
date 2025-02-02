fc18911a1dff56024616ca593726101b90a9b676
gunyongbok <qhrrjsdyd123@gmail.com>
2025-02-02 16:49:47 +0900

    feat: Apply latest changes before merging develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Outlet } from "react-router-dom";
-import HomePage from "./pages/home";
+import { Outlet } from 'react-router-dom';
+import HomePage from './pages/home';
 
 const App = () => {
   return (
-    <div className="flex justify-center">
-      <div className="w-[1440px] max-w-full">
-        <HomePage />
-      </div>
+    <div className="flex flex-col items-center web:w-[1440px] web:mx-auto mobile:max-w-[768px]">
+      <HomePage />
     </div>
   );
 };
