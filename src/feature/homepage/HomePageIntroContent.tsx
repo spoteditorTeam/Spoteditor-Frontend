88500f869d7149774a525fb05f2eaec78de3422c
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-03 21:31:12 +0900

    feat: 홈페이지 인트로에 도시 검색 섹션 추가 및 레이아웃 수정
    - 인트로 콘텐츠에 도시 검색 섹션 추가
    - 인트로 내부 요소들의 레이아웃 일부 수정

--- src/feature/homepage/HomePageIntroContent.tsx
+++ src/feature/homepage/HomePageIntroContent.tsx
@@ -1,5 +1,6 @@
 import { useSearchStore } from '@/store/searchStore';
 import ui from '../../assets/homepage/ui-spoteditor.png';
+import CitySearchForm from './CitySearchForm';
 
 const HomePageIntroContent = () => {
   const category: string[] = [
@@ -16,17 +17,17 @@ const HomePageIntroContent = () => {
     <>
       {!isOpen ? (
         <div className="w-full flex pt-24 py-10 h-[387px] gap-7 bg-black px-50px">
-          <div className="w-[655px] flex flex-col gap-9 bg-red-300">
-            <div className="text-white font-pretendard text-32 font-500">
+          <div className="w-[655px] flex flex-col justify-between gap-9">
+            <div className="text-white font-pretendard text-28 font-500">
               반가워요!
               <br />
               Spoteditor는 <span className="font-semibold">"어디 가서 놀지?"</span> 하고 고민하는
               <br />
               여러분을 위해 만들어졌어요.
             </div>
-            <section className="w-full bg-blue-300"></section>
+            <CitySearchForm />
           </div>
-          <div className="w-[655px] flex flex-col gap-5">
+          <div className="w-[655px] flex flex-col gap-5 justify-between">
             <img src={ui} alt="ui_spoteditior_description" />
             <div className="flex flex-wrap gap-1.5">
               {category.map((i) => {
