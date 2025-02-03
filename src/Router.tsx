5c9a4fdc36c0c6d35cdcddeb79b44b94116fc1f0
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-03 20:51:51 +0900

    feat: 검색 결과를 보여주는 Search 페이지 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/home';
+import Search from './pages/search';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: 'search',
+        element: <Search />,
       },
     ],
   },
