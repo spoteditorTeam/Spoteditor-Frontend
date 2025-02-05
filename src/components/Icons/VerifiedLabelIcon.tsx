ef3a05ff15c3290cf4889f56c0d4e656bf57cf8f
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-06 03:30:59 +0900

    fix: VerifiedLabelIcon이 테일윈드 크기 조정을 반영하지 않는 문제 수정
    - width="15" height="15"을 제거하고 width="100%" height="100%"로 변경
    - className의 w-* h-* 속성이 정상적으로 적용되지 않던 문제 해결
    - 테일윈드를 사용한 크기 조절이 가능하도록 수정

--- src/components/Icons/VerifiedLabelIcon.tsx
+++ src/components/Icons/VerifiedLabelIcon.tsx
@@ -6,10 +6,10 @@ function VerifiedLabelIcon({ className }: VerifiedLabelIconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="15"
-      height="15"
       viewBox="0 0 15 15"
       fill="none"
+      width="100%"
+      hanging="100%"
       className={className}
     >
       <path
