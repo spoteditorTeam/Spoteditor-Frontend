8b5353bff6b360ecc2dac6d8db2fcfe24f5e609c
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-09 22:08:35 +0900

    feat: 메인 헤더 로고 클릭 시 홈 이동

--- src/components/Header/MainHeader/MainHeader.tsx
+++ src/components/Header/MainHeader/MainHeader.tsx
@@ -4,13 +4,16 @@ import global from '@/assets/homepage/global.png';
 import LoginModal from '../../../feature/login/LoginModal';
 import { useSearchStore } from '@/store/searchStore';
 import LoginStatusButtons from './LoginStatusButtons';
+import { Link } from 'react-router-dom';
 
 const MainHeader = () => {
   const { toggleSearchBar } = useSearchStore();
 
   return (
     <header className="w-full bg-black z-30 web:py-[20px] web:px-[50px] mobile:py-[4px] mobile:px-[16px] flex justify-between">
-      <img src={logo} alt="logo" className="object-contain" />
+      <Link to={'/'}>
+        <img src={logo} alt="logo" className="object-contain" />
+      </Link>
       <section className="flex web:gap-[46px] mobile:gap-[20px] items-center justify-between text-white">
         <button onClick={toggleSearchBar}>
           <img src={search} alt="search_logo" className="object-contain w-[20px] h-[20px]" />
