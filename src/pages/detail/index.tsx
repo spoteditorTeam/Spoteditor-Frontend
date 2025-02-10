025130c18cffb12a200e81ba51ec6c1075fba18e
hayanLee <hayanlee.dev@gmail.com>
2025-02-10 18:39:42 +0900

    feat : 디테일 페이지 (웹) 스타일링 구현

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -26,26 +26,28 @@ const DetailPage = () => {
         </div>
       </div>
 
-      <div className="flex flex-col px-4 py-2.5 gap-[15px]">
-        {/* 프로필  */}
-        <div className="flex items-center gap-2">
-          <Avatar className="w-6 h-6">
-            <AvatarImage src="https://github.com/shadcn.png" alt="user Avatar" />
-          </Avatar>
+      <div className="flex flex-col px-4 py-2.5 gap-[15px] web:px-[50px]">
+        <div className="web:grid web:grid-cols-[1fr_3fr] web:gap-5">
+          {/* 프로필  */}
+          <div className="flex items-center gap-2">
+            <Avatar className="w-6 h-6">
+              <AvatarImage src="https://github.com/shadcn.png" alt="user Avatar" />
+            </Avatar>
 
-          <p className="text-text-sm font-semibold">Teamspoteditor</p>
-          <SubstractIcon fill="black" />
+            <p className="text-text-sm font-semibold">Teamspoteditor</p>
+            <SubstractIcon fill="black" />
 
-          <Button className="rounded-full py-1 px-4 bg-gray-50 text-black">팔로잉</Button>
+            <Button className="rounded-full py-1 px-4 bg-gray-50 text-black">팔로잉</Button>
+          </div>
+
+          {/* 설명 */}
+          <p className="text-primary-400 text-text-sm web:grow web:text-text-lg web:py-1.5">
+            그저 그날의 날씨, 공간의 분위기, 흘러나오는 음악 소리, 그리고 함께 있는 사람, 그 모든
+            것들이 조화롭게 어우러지는 순간. 그럴 때 커피 한 잔이 우리의 감정을 담는 그릇이
+            되어특별한 의미를 갖는 것 같아요. 풍류는 옛 선비들이 인격 수양을 위해 자연을 가까이 두고
+            멋스럽게 운치를 즐기던 행위를 뜻합니다. 어느 학자는 이런 의미를 두고 멋스럽게
+          </p>
         </div>
-
-        {/* 설명 */}
-        <p className="text-primary-400 text-text-sm">
-          그저 그날의 날씨, 공간의 분위기, 흘러나오는 음악 소리, 그리고 함께 있는 사람, 그 모든
-          것들이 조화롭게 어우러지는 순간. 그럴 때 커피 한 잔이 우리의 감정을 담는 그릇이 되어특별한
-          의미를 갖는 것 같아요. 풍류는 옛 선비들이 인격 수양을 위해 자연을 가까이 두고 멋스럽게
-          운치를 즐기던 행위를 뜻합니다. 어느 학자는 이런 의미를 두고 멋스럽게
-        </p>
 
         {/* 컨텐츠 */}
         <PlaceItem />
