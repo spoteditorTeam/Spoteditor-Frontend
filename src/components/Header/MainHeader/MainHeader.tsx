983becbd9f67d9e6a38db557b70468764b1a46f2
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-15 18:34:37 +0900

    fix: 모달창 오픈 시 스크롤바 사라짐에 따른 헤더 너비 변경 문제 수정
     - 모달창 오픈 시 스크롤바가 사라지면서 MainHeader의 너비가 늘어나는 문제 수정
     - MainLayout의 부모 요소에 h-auto를 적용하여 스크롤 시 헤더가 사라지는 현상 방지

--- src/components/Header/MainHeader/MainHeader.tsx
+++ src/components/Header/MainHeader/MainHeader.tsx
@@ -10,7 +10,7 @@ const MainHeader = () => {
   const { toggleSearchBar } = useSearchStore();
 
   return (
-    <header className="fixed w-full z-50 bg-black web:px-[50px] mobile:px-4 web:py-5 mobile:py-[14px] web:min-w-[1440px] flex justify-center flex-col left-0 top-0">
+    <header className="sticky w-full z-50 bg-black web:px-[50px] mobile:px-4 web:py-5 mobile:py-[14px] web:min-w-[1440px] flex justify-center flex-col left-0 top-0">
       <section className="flex justify-between w-full">
         <Link to="/">
           <LogoIcon className="text-white w-[120px] h-5 cursor-pointer" fill="white" />
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ import MainHeader from '../components/Header/MainHeader/MainHeader';
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col items-center h-screen web:min-w-[1440px] web:pt-[60px] mobile:pt-10">
+    <div className="flex flex-col items-center h-auto min-h-screen web:min-w-[1440px] w-full">
       <MainHeader />
       <div className="w-full grow">
         <Outlet />
