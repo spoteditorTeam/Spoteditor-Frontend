ff5c53a61e433cd9299a7ec0e44f9c081f1646e1
hayanLee <hayanlee.dev@gmail.com>
2025-02-17 16:47:10 +0900

    fix : 메인페이지 hero section 수정
    - 토글 시 설명 펼침/닫힘

--- src/features/homepage/CitySearchForm.tsx
+++ src/features/homepage/CitySearchForm.tsx
@@ -2,37 +2,35 @@ import { Button } from '@/components/ui/button';
 
 function CitySearchForm() {
   return (
-    <section>
-      <form className="grid grid-rows-2 gap-2.5 web:flex web:h-full">
-        <div className="flex gap-[6px] grow">
-          <div className="flex flex-col grow bg-white px-3 py-2.5 gap-2">
-            <label htmlFor="sido" className="text-primary-400 text-text-sm">
-              어디로 놀러갈까요?
-            </label>
-            <input
-              id="sido"
-              type="text"
-              placeholder="서울"
-              className="grow text-black text-text-md font-bold placeholder:text-black web:text-sm"
-            />
-          </div>
-          <div className="flex flex-col grow bg-white px-3 py-2.5 gap-2">
-            <label htmlFor="gu" className="text-primary-400 text-text-sm">
-              더 상세히 검색!
-            </label>
-            <input
-              id="gu"
-              type="text"
-              placeholder="종로구, 중구"
-              className="grow text-black text-text-md font-bold placeholder:text-black web:text-sm"
-            />
-          </div>
+    <form className="flex flex-col gap-2.5 web:grid web:grid-cols-[3fr_70px] ">
+      <div className="grid grid-cols-2 gap-1.5">
+        <div className="flex flex-col bg-white px-3 py-2.5 gap-2">
+          <label htmlFor="sido" className="text-primary-400 text-text-sm">
+            어디로 놀러갈까요?
+          </label>
+          <input
+            id="sido"
+            type="text"
+            placeholder="서울"
+            className="grow text-black text-text-md font-bold placeholder:text-black web:text-sm"
+          />
         </div>
-        <Button className="bg-primary-950 text-text-sm text-white rounded-none web:h-full">
-          검색
-        </Button>
-      </form>
-    </section>
+        <div className="flex flex-col bg-white px-3 py-2.5 gap-2">
+          <label htmlFor="gu" className="text-primary-400 text-text-sm">
+            더 상세히 검색!
+          </label>
+          <input
+            id="gu"
+            type="text"
+            placeholder="종로구, 중구"
+            className="grow text-black text-text-md font-bold placeholder:text-black web:text-sm"
+          />
+        </div>
+      </div>
+      <Button className="bg-primary-950 text-text-sm font-medium text-white rounded-none" asChild>
+        <p className="h-full">검색</p>
+      </Button>
+    </form>
   );
 }
 
