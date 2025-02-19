8a36609c1fa4ab3b212ad5c26d585e11917f61cb
hayanLee <hayanlee.dev@gmail.com>
2025-02-20 02:46:41 +0900

    feat : 카카오맵 연결

--- src/pages/register/MapPage.tsx
+++ src/pages/register/MapPage.tsx
@@ -1,9 +1,34 @@
+import { Button } from '@/components/ui/button';
 import RegisterSearchBar from '@/features/registerpage/RegisterSearchBar';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+// 카카오맵 kakao 객체 타입 선언
+const kakao = (window as any).kakao;
 
 const MapPage = () => {
+  useEffect(() => {
+    kakao.maps.load(() => {
+      const container = document.getElementById('map');
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+      };
+      new kakao.maps.Map(container, options);
+    });
+  }, []);
+
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <RegisterSearchBar />
+
+      {/* 지도 담을 영역 */}
+      <div id="map" className="w-full h-full"></div>
+
+      <div className="px-4 mb-6 pt-2">
+        <Button className="w-full" asChild size={'xl'}>
+          <Link to={'#'}>선택</Link>
+        </Button>
+      </div>
     </div>
   );
 };
