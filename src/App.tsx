5b352415fb7dabc0f5502683e7e7ea28c256621b
hayanLee <hayanlee.dev@gmail.com>
2025-03-01 00:05:13 +0900

    chore : ReactQueryDevtools 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router';
-
 const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
