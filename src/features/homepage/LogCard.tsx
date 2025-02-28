2178484c5f378c0337ce650a21a5d874e9c5bed2
hayanLee <hayanlee.dev@gmail.com>
2025-03-01 00:48:44 +0900

    feat : 홈페이지 로그 쿼리 연결

--- src/features/homepage/LogCard.tsx
+++ src/features/homepage/LogCard.tsx
@@ -1,38 +1,35 @@
 import { BookMarkIcon, SubtractIcon } from '@/components/Icons';
 import { cn } from '@/lib/utils';
+import { LogContent } from '@/services/apis/types/logAPI.type';
+import { useNavigate } from 'react-router-dom';
 
 type LogCardProps = {
-  title: string;
-  image: string;
-  location1: string;
-  location2: string;
   isLarge?: boolean;
   vertical?: boolean;
   isModal?: boolean;
+  log?: LogContent;
 };
 
-const LogCard = ({
-  title,
-  image,
-  location1,
-  location2,
-  isLarge,
-  vertical,
-  isModal,
-}: LogCardProps) => {
+const LogCard = ({ isLarge, vertical, isModal, log }: LogCardProps) => {
+  const navi = useNavigate();
+  const handleCardClick = () => navi(`/log/${log?.placeLogId}`);
   return (
     <div
       className={cn(
         'grid gap-1.5 h-full grid-rows-[auto_1fr]',
         isLarge && 'web:grid-rows-[8.5fr_1fr]'
       )}
+      onClick={handleCardClick}
     >
       {/* 이미지 */}
       <div className={cn('relative grow')}>
         <img
-          src={image}
+          src={log?.image.storedFile}
           alt="장소 이미지"
-          className={cn('object-cover w-full h-full aspect-[3/2]', vertical && 'aspect-[3/4]')}
+          className={cn(
+            'object-cover w-full h-full aspect-[3/2] bg-blue-100',
+            vertical && 'aspect-[3/4]'
+          )}
         />
         {!isModal && (
           <span className="flex items-center gap-1 p-2.5 text-white text-text-2xs font-semibold absolute bottom-0">
@@ -49,9 +46,9 @@ const LogCard = ({
 
       {/* 설명 */}
       <div className="text-text-sm web:text-text-md">
-        <h5 className="font-bold">{title}</h5>
+        <h5 className="font-bold">{log?.name}</h5>
         <h6 className="text-primary-300 font-normal">
-          {location1} | {location2}
+          {log?.address.sido} | {log?.address.sigungu}
         </h6>
       </div>
     </div>
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,7 @@
 import MainLayout from '@/layouts/MainLayout';
 import RegisterLayout from '@/layouts/RegisterLayout';
 import DetailPage from '@/pages/detail';
-import HomePage from '@/pages/home';
+import HomePage from '@/pages/home/HomePage';
 import Notice from '@/pages/notice';
 import NoticeDetail from '@/pages/notice/notice-detail';
 import Profile from '@/pages/profile';
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/search', element: <Search /> },
-      { path: '/detail/:placeId', element: <DetailPage /> },
+      { path: '/log/:placeId', element: <DetailPage /> },
       {
         path: '/profile/:userId',
         element: <Profile />,
