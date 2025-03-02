ba2c6b9cce59f78271db559c2bb71dbb0cdd455c
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-02 20:29:15 +0900

    style: 공지사항 페이지 디자인 수정
     - 일부 레이아웃 수정
     - 공지사항 페이지는 헤더와 푸터가 안 보이도록 수정
     - 공지사항 상세페이지 제목 부분 디자인대로 수정
     - 공지사항 상세페이지는 스크롤 시 제목의 이동을 확인하기 위해 임의로 heigh를 300vh로 설정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,14 @@
-import MainFooter from '@/components/Footer/MainFooter';
 import { Outlet } from 'react-router-dom';
-import MainHeader from '../components/Header/MainHeader/MainHeader';
+import ContentLayout from '@/layouts/ContentLayout';
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen web:min-w-[1440px] w-ful">
-      <MainHeader />
-      <div className="w-full grow">
-        <Outlet />
-      </div>
-      <MainFooter />
+    <div className="flex flex-col items-center web:min-w-[1440px] w-full">
+      <ContentLayout>
+        <div className="w-full grow">
+          <Outlet />
+        </div>
+      </ContentLayout>
     </div>
   );
 };
