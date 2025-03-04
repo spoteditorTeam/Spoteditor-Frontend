a29f1d924679a49c457a4edc8c41e30b7e741a04
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-05 02:55:55 +0900

    useUpdateUser 훅 추가
    유저 프로필 업로드를 할 때 사용

---
+++ src/hooks/queries/user/useUpdateUser.ts
@@ -0,0 +1,18 @@
+import { IUpdateUser } from '@/services/apis/types/userAPI.type';
+import { authUserApi } from '@/services/apis/userApi';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { userKeys } from '@/hooks/queries/user/userQueryKeys';
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: IUpdateUser) => authUserApi.patchUser(data),
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: userKeys.all });
+    },
+    onError(err) {
+      console.error('프로필 업데이트 실패:', err);
+    },
+  });
+}
