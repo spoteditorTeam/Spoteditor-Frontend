7c8485b73045947464ca082794944108912cc9f9
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-06 04:50:26 +0900

    feat: 유저로그 도메인 패칭함수 클래스 추가

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import LogAPI from './logApi';
 import PlaceAPI from './placeApi';
 import RegisterAPI from './registerApi';
+import UserLog from './userLogAPI';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -9,11 +10,13 @@ class API {
   #axios;
   register;
   log;
+  userLog;
   place;
   constructor() {
     this.#axios = axios.create({ baseURL: API_BASE_URL, withCredentials: true });
     this.register = new RegisterAPI(this.#axios);
     this.log = new LogAPI(this.#axios);
+    this.userLog = new UserLog(this.#axios);
     this.place = new PlaceAPI(this.#axios);
   }
 }
---
+++ src/services/apis/types/userLogAPI.type.ts
@@ -0,0 +1,60 @@
+import { LogContent, LogsQueryParams } from './logAPI.type';
+import { Address, Image } from './registerAPI.type';
+/* 타유저 api 파라미터 */
+export interface IOtherUserParams {
+  userId: number;
+  params: LogsQueryParams;
+}
+
+interface SortInfo {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+interface Place {
+  placeId: number;
+  author: string;
+  name: string;
+  description: string;
+  address: Address;
+  category: string;
+}
+
+interface PlaceLog {
+  placeLogId: number;
+  name: string;
+  image: Image;
+  address: Address;
+  views: number;
+}
+
+/* 유저 로그리스트 응답*/
+export interface UserLogs {
+  content: LogContent[];
+  totalCount: number;
+  pageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  sort: SortInfo;
+}
+
+/* 유저 북마크 장소 응답*/
+export interface UserBookmarkPlaces {
+  content: Place[];
+  totalCount: number;
+  pageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  sort: SortInfo;
+}
+
+/* 유저 북마크 로고리스트 응답 */
+export interface UserBookmarkLogs {
+  content: PlaceLog[];
+  totalCount: number;
+  pageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  sort: SortInfo;
+}
