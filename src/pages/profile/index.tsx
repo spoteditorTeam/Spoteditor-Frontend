6e1f0af82b00683319de743a1a6549c09cd62b73
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-06 18:30:36 +0900

    feat: 현재 경로가 '/profile/:userId'일 경우 /profile/:userId/my-logs으로 이동

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,25 @@
 import ProfileHeader from '@/features/profile/ProfileHeader';
 import TapNavigation from '@/features/profile/TapNavigation';
+import useUser from '@/hooks/queries/user/useUser';
 import PageLayout from '@/layouts/PageLayout';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
 
 function Profile() {
+  const nav = useNavigate();
+  const { pathname } = useLocation();
+  const { userId } = useParams();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user?.userId === userId) {
+      nav('/');
+    }
+    /* 현재 경로가 '/profile/:userId'일 경우 my-logs으로 이동 */
+    if (pathname.match(/^\/profile\/\d+$/)) {
+      nav(`/profile/${userId}/my-logs`);
+    }
+  }, [user, userId, pathname]);
   return (
     <div className="flex w-full flex-col gap-[50px] items-center">
       <PageLayout>
