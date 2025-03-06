fb9d38f0ed4196fa25a324884073763148de4e02
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-06 21:48:21 +0900

    feat: 마이페이지 PostCard 스켈레톤 적용

--- src/components/Skeleton/PostCardSkeleton.tsx
+++ src/components/Skeleton/PostCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '../ui/skeleton';
 
 export default function PostCardSkeleton() {
   return (
-    <section className="grid w-full grid-cols-1 web:grid-cols-4 gap-[34px] web:gap-x-[15px] web:gap-y-[40px]">
+    <section className="grid w-full grid-cols-1 web:grid-cols-4 gap-[34px] web:gap-x-[15px] web:gap-y-[40px] mb-[50px]">
       {Array.from({ length: 12 }).map((_, idx) => (
         <article key={idx} className="flex flex-col w-full gap-1.5">
           <Skeleton className="w-full  aspect-[324/218]" />
--- src/pages/profile/my-logs/index.tsx
+++ src/pages/profile/my-logs/index.tsx
@@ -1,4 +1,5 @@
 import MainPagination from '@/components/Pagination/MainPagination';
+import PostCardSkeleton from '@/components/Skeleton/PostCardSkeleton';
 import {
   PostCard,
   PostCardImage,
@@ -6,22 +7,44 @@ import {
   PostCardTitle,
   PostCardWrapper,
 } from '@/features/profile/PostCard';
+import useUserLogs from '@/hooks/queries/userLog/useUserLogs';
 
 function MyLogs() {
+  const { data, isLoading } = useUserLogs();
+  console.log(data);
+
   return (
     <>
-      <PostCardWrapper className="mb-[50px]">
-        {Array.from({ length: 12 }).map((_, idx) => (
-          <PostCard key={idx}>
-            <PostCardImage lable className="bg-slate-300" />
-            <PostCardTitle title="혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페" />
-            <PostCardLocation location="서울" detail="위치 세부 정보" />
-          </PostCard>
-        ))}
-      </PostCardWrapper>
-      <MainPagination />
+      {isLoading ? (
+        <PostCardSkeleton />
+      ) : (
+        <PostCardWrapper className="mb-[50px]">
+          {data?.content.map((log) => (
+            <PostCard key={log.placeLogId} className="bg-blue-300">
+              <PostCardImage lable className="bg-slate-300" imageUrl="" />
+              <PostCardTitle title={log.name} />
+              <PostCardLocation location={log.address.sido} detail={log.address.sigungu} />
+            </PostCard>
+          ))}
+        </PostCardWrapper>
+      )}
+      <section className="mt-[50px]">
+        <MainPagination />
+      </section>
     </>
   );
 }
 
 export default MyLogs;
+
+{
+  /* <PostCardWrapper className="mb-[50px]">
+  {Array.from({ length: 12 }).map((_, idx) => (
+    <PostCard key={idx} className="bg-blue-300">
+      <PostCardImage lable className="bg-slate-300" />
+      <PostCardTitle title="혼자 보내는 하루, 골목골목 숨어있는 용산 원효로 카페" />
+      <PostCardLocation location="서울" detail="위치 세부 정보" />
+    </PostCard>
+  ))}
+</PostCardWrapper>; */
+}
