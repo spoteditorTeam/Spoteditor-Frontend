91caa3e5929fafba8f9ffcce3266d63e3e0eb31f
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-06 18:09:31 +0900

    refactor: 유저 로그 도메인 api 훅의 파라미터에 기본값 추가

--- src/hooks/queries/userLog/useOtherUserLogs.ts
+++ src/hooks/queries/userLog/useOtherUserLogs.ts
@@ -3,9 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 import { userLogsKeys } from '@/hooks/queries/userLog/userLogQueryKeys';
 import api from '@/services/apis/api';
 
-export default function useOtherUserLogs(userId: number, params: LogsQueryParams) {
+export default function useOtherUserLogs(
+  userId: number,
+  { page = 1, size = 12, direction = 'ASC' }: Partial<LogsQueryParams> = {}
+) {
   return useQuery({
-    queryKey: userLogsKeys.otherUserLogList(userId, params),
-    queryFn: () => api.userLog.getOtherUserLogs({ userId, params }),
+    queryKey: userLogsKeys.otherUserLogList(userId, { page, size, direction }),
+    queryFn: () => api.userLog.getOtherUserLogs({ userId, params: { page, size, direction } }),
   });
 }
--- src/hooks/queries/userLog/useUserBookmarkPlaces.ts
+++ src/hooks/queries/userLog/useUserBookmarkPlaces.ts
@@ -3,9 +3,13 @@ import { useQuery } from '@tanstack/react-query';
 import { userLogsKeys } from '@/hooks/queries/userLog/userLogQueryKeys';
 import api from '@/services/apis/api';
 
-export default function useUserBookmarkPlaces(params: LogsQueryParams) {
+export default function useUserBookmarkPlaces({
+  page = 1,
+  size = 12,
+  direction = 'ASC',
+}: Partial<LogsQueryParams> = {}) {
   return useQuery({
-    queryKey: userLogsKeys.bookmarkPlaceList(params),
-    queryFn: () => api.userLog.getUserBookmarkPlaces(params),
+    queryKey: userLogsKeys.bookmarkPlaceList({ page, size, direction }),
+    queryFn: () => api.userLog.getUserBookmarkPlaces({ page, size, direction }),
   });
 }
