d6981eabeb01a8081a37f22438d8a952454e7cae
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-07 18:49:32 +0900

    feat: 커스텀 페이지네이션 컴포넌트 추가

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -1,56 +1,73 @@
 import {
   Pagination,
   PaginationContent,
-  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
-  PaginationNext,
   PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
 } from '@/components/ui/pagination';
-import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 interface CustomPaginationProps {
-  total: number;
-  current: number;
-  className?: string;
+  currentPage: number;
+  totalPages: number;
 }
 
-function CustomPagination({ total, current, className = '' }: CustomPaginationProps) {
-  const [pageNums] = useState([1, 2, 3]);
+export default function CustomPagination({ currentPage, totalPages }: CustomPaginationProps) {
+  const [_, setSearchParams] = useSearchParams();
 
-  const onPrevClick = () => {};
+  const handlePageChange = (page: number) => {
+    setSearchParams({ pageNumber: page.toString(), totalPages: totalPages.toString() });
+  };
 
-  const onNextClick = () => {};
+  const shouldShowLeftArrow = currentPage > 3;
+  const shouldShowRightArrow = currentPage < totalPages - 2;
+
+  const getPageNumbers = () => {
+    if (totalPages <= 5) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+    if (currentPage >= totalPages - 3) {
+      return [totalPages - 4, totalPages - 3, totalPages - 2, totalPages - 1, totalPages];
+    }
+    if (currentPage >= totalPages - 2) {
+      return [1, '...', totalPages - 2, totalPages - 1, totalPages];
+    }
+    if (currentPage > 3 && totalPages - 3) {
+      return [currentPage - 1, currentPage, currentPage + 1, '...', totalPages];
+    }
+
+    return [1, 2, 3, '...', totalPages];
+  };
   return (
-    <Pagination className={className}>
+    <Pagination>
       <PaginationContent>
-        {current >= 1 || current <= 3 ? (
+        {shouldShowLeftArrow && (
           <PaginationItem>
-            <PaginationPrevious onClick={onPrevClick} />
+            <PaginationPrevious onClick={() => handlePageChange(currentPage - 1)} />
           </PaginationItem>
-        ) : null}
-        {pageNums.map((num) => (
+        )}
+        {getPageNumbers().map((page, idx) =>
+          Number.isInteger(page) ? (
+            <PaginationItem key={idx}>
+              <PaginationLink
+                onClick={() => handlePageChange(Number(page))}
+                isActive={currentPage === page}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          ) : (
+            <PaginationEllipsis />
+          )
+        )}
+        {shouldShowRightArrow && (
           <PaginationItem>
-            <PaginationLink isActive={current === num}>{num}</PaginationLink>
+            <PaginationNext onClick={() => handlePageChange(currentPage + 1)} />
           </PaginationItem>
-        ))}
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="">{total}</PaginationLink>
-        </PaginationItem>
-        {current >= total - 3 && current <= total ? (
-          <PaginationItem>
-            <PaginationPrevious onClick={onPrevClick} />
-          </PaginationItem>
-        ) : null}
-        <PaginationItem>
-          <PaginationNext onClick={onNextClick} />
-        </PaginationItem>
+        )}
       </PaginationContent>
     </Pagination>
   );
 }
-
-export default CustomPagination;
