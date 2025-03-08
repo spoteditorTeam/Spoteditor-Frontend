22df35b6676226fc7d4cb7bd52b6e73cc1ec0103
hayanLee <hayanlee.dev@gmail.com>
2025-03-08 20:35:00 +0900

    feat : 로그 작성 시 사진 필수 유효성 검사 추가

--- src/pages/register/LogWritePage.tsx
+++ src/pages/register/LogWritePage.tsx
@@ -1,4 +1,3 @@
-import { CameraIcon } from '@/components/Icons';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -14,29 +13,26 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import CoverImageInput from '@/features/registerpage/CoverImageInput';
 import LogWriteBar from '@/features/registerpage/LogWriteBar';
 import PlaceDetailFormItem from '@/features/registerpage/PlaceDetailFormItem';
 import useImagePreview from '@/hooks/useImagePreview';
-import { cn } from '@/lib/utils';
 import api from '@/services/apis/api';
-import { Address, Log, Place, PresignedUrlWithName } from '@/services/apis/types/registerAPI.type';
+import { Log, Place, PresignedUrlWithName } from '@/services/apis/types/registerAPI.type';
 import { useRegisterStore } from '@/store/registerStore';
+import { formatAddress } from '@/utils/formatLogForm';
 import { CircleX } from 'lucide-react';
 import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// 스크롤 수정 필요
 const LogWritePage = () => {
   const navi = useNavigate();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [logTitle, setLogTitle] = useState('');
-  const { imagePreview, handleFileChange, handleClearImage, presignedUrlObj } = useImagePreview();
   const selectedPlaces = useRegisterStore((state) => state.selectedPlaces);
   const resetSelectedPlaces = useRegisterStore((state) => state.resetSelectedPlaces);
-
-  const coverUploadInputRef = useRef<HTMLInputElement>(null);
+  const { imagePreview, handleFileChange, handleClearImage, presignedUrlObj } = useImagePreview();
   const logDescripTextAreaRef = useRef<HTMLTextAreaElement>(null);
-  const textRefs = useRef<{ [placeId: string]: string }>({});
 
   // 로그 등록 시 필요한 {presignedUrl, uuid}
   const [presignedUrlList, setPresignUrlList] = useState<{ [key: string]: PresignedUrlWithName[] }>(
@@ -45,32 +41,31 @@ const LogWritePage = () => {
 
   const handleClearTitle = () => setLogTitle('');
 
-  const setRef = (id: string, elem: HTMLTextAreaElement) => {
+  const textRefs = useRef<{ [placeId: string]: string }>({});
+  const registerTextRef = (id: string, elem: HTMLTextAreaElement) => {
     if (elem) textRefs.current[id] = elem.value;
     else delete textRefs.current[id];
   };
 
   const [sido, , bname] = selectedPlaces[0].address_name.split(' '); // 뒤로가기 옆 로그 대표 지역 이름
-
   // 제출 형식에 맞춰 포맷
-  const formatAddress = (place: kakao.maps.services.PlacesSearchResultItem): Address => ({
-    address: place.address_name,
-    roadAddress: place.road_address_name,
-    latitude: Number(place.y),
-    longitude: Number(place.x),
-    sido: place.address_name.split(' ')[0],
-    bname: place.address_name.split(' ')[1],
-    sigungu: place.address_name.split(' ')[2],
-  });
 
-  const formatPlace = (place: kakao.maps.services.PlacesSearchResultItem): Place => ({
-    name: place.place_name,
-    description: textRefs.current[place.id],
-    address: formatAddress(place),
-    category: 'TOUR',
-    originalFiles: presignedUrlList[place.place_name].map((item) => item.originalFile),
-    uuids: presignedUrlList[place.place_name].map((item) => item.uuid),
-  });
+  const formatPlace = (place: kakao.maps.services.PlacesSearchResultItem): Place | null => {
+    const placeImages = presignedUrlList[place.place_name];
+    if (!placeImages || placeImages.length === 0) {
+      alert(`${place.place_name} 이미지가 비어있습니다.`);
+      return null;
+    }
+
+    return {
+      name: place.place_name,
+      description: textRefs.current[place.id],
+      address: formatAddress(place),
+      category: 'TOUR',
+      originalFiles: presignedUrlList[place.place_name].map((item) => item.originalFile),
+      uuids: presignedUrlList[place.place_name].map((item) => item.uuid),
+    };
+  };
 
   const formatLog = (places: kakao.maps.services.PlacesSearchResult): Log | null => {
     if (!logTitle || !logDescripTextAreaRef.current?.value || !presignedUrlObj?.originalFile) {
@@ -78,16 +73,19 @@ const LogWritePage = () => {
       return null;
     }
 
+    const formattedPlaces = places.map((place) => formatPlace(place));
+
+    if (formattedPlaces.some((place) => place === null)) return null;
+
+    // 로그 정보
     return {
       name: logTitle,
-      description: logDescripTextAreaRef.current?.value,
+      description: logDescripTextAreaRef.current.value,
       originalFile: presignedUrlObj?.originalFile,
       uuid: presignedUrlObj?.uuid,
       status: 'public',
       tags: [],
-      places: places.map((place) => ({
-        ...formatPlace(place),
-      })),
+      places: formattedPlaces as Place[],
     };
   };
 
@@ -107,9 +105,9 @@ const LogWritePage = () => {
       {/* 헤더 */}
       <LogWriteBar sido={sido} bname={bname} />
 
-      <main className="flex flex-col items-center grow gap-3 min-h-0 overflow-y-auto scrollbar-hide">
-        {/* 로그 */}
-        <div className="flex items-center w-full border-b px-4 relative">
+      <main className="flex flex-col items-center grow min-h-0 overflow-y-auto scrollbar-hide">
+        {/* 로그 제목 */}
+        <section className="flex items-center w-full border-b px-4 relative">
           <Input
             name="logTitle"
             placeholder="제목을 입력해주세요. (최대 30자) *"
@@ -120,51 +118,21 @@ const LogWritePage = () => {
           />
           {logTitle && (
             <CircleX
-              className="stroke-primary-300 fill-slate-100 stroke-1 absolute top-2 right-2  cursor-pointer hover:fill-slate-50/50"
+              className="stroke-white fill-primary-100 absolute stroke-1 top-2 right-4  cursor-pointer hover:fill-slate-50/50"
+              size={24}
               onClick={handleClearTitle}
             />
           )}
-        </div>
+        </section>
 
         {/* 커버 이미지 */}
-        {imagePreview && (
-          <div className="relative mb-2.5">
-            <Input
-              id="coverImg"
-              type="image"
-              src={imagePreview}
-              alt="커버 이미지"
-              className="w-full aspect-[2/1] p-0 object-cover"
-            />
-            <CircleX
-              className="stroke-primary-100 absolute top-4 right-4 cursor-pointer hover:fill-slate-50/50"
-              onClick={handleClearImage}
-            />
-          </div>
-        )}
-        <div className="px-4 w-full web:px-0">
-          {/* 파일 입력 */}
-          <Input
-            type="file"
-            accept="image/*"
-            onChange={handleFileChange}
-            ref={coverUploadInputRef}
-            className="hidden"
-          />
-          {/* 커버 이미지 버튼 */}
-          <Button
-            variant={'outline'}
-            className={cn(
-              'border w-full border-dashed gap-[5px] text-primary-600 px-2.5 py-3 mb-2.5',
-              imagePreview && 'hidden'
-            )}
-            onClick={() => coverUploadInputRef.current?.click()}
-          >
-            <CameraIcon className="stroke-primary-600" />
-            <span className="text-text-sm font-bold">
-              커버이미지<span className="text-error-600 m-1">*</span>
-            </span>
-          </Button>
+        <CoverImageInput
+          imagePreview={imagePreview}
+          handleFileChange={handleFileChange}
+          handleClearImage={handleClearImage}
+        />
+
+        <div className="px-4 w-full">
           {/* 로그 설명 */}
           <Textarea
             className="bg-primary-50 min-h-[85px] px-[18px] py-2.5 text-primary-300 text-text-sm  placeholder:text-primary-300 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
@@ -173,13 +141,14 @@ const LogWritePage = () => {
             ref={logDescripTextAreaRef}
           />
 
+          {/* 장소 */}
           <div className="flex flex-col w-full mt-3">
             {selectedPlaces.map((place, idx) => (
               <PlaceDetailFormItem
                 place={place}
                 key={place.id}
                 idx={idx + 1}
-                registerTextRef={setRef}
+                registerTextRef={registerTextRef}
                 onChangePresignUrlList={setPresignUrlList}
               />
             ))}
