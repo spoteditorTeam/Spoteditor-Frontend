9c8329765fbd45e428d27eb4a858c6d804cc9d9d
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-08 16:36:59 +0900

    fix: 쿼리 훅의 useQueryOptions 반환 타입에러 해결
     - 쿼리 훅의 반환타입이 unknown이 되는 에러 발생
     - useQueryOptions 타입을 사용할 때 queryFn의 반환 타입을 자동으로 추론하지 않으므로 unknown을 반환

--- src/hooks/queries/userLog/useUserLogs.ts
+++ src/hooks/queries/userLog/useUserLogs.ts
@@ -2,12 +2,13 @@ import { LogsQueryParams } from '@/services/apis/types/logAPI.type';
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { userLogsKeys } from '@/hooks/queries/userLog/userLogQueryKeys';
 import api from '@/services/apis/api';
+import { UserLogs } from '@/services/apis/types/userLogAPI';
 
 export default function useUserLogs(
   { page = 1, size = 12, direction = 'ASC' }: Partial<LogsQueryParams> = {},
-  queryOptions?: Partial<UseQueryOptions>
+  queryOptions?: Partial<UseQueryOptions<UserLogs, Error>>
 ) {
-  return useQuery({
+  return useQuery<UserLogs, Error>({
     queryKey: userLogsKeys.logList({ page, size, direction }),
     queryFn: () => api.userLog.getUserLogs({ page, size, direction }),
     ...queryOptions,
