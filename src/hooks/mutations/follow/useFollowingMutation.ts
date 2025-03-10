5af32318b35b4a646fa7785f677669b2d4dc0bbf
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-10 17:34:57 +0900

    refactor: 팔로잉 뮤테이션 부분 리팩토링
    타유저 조회 api의 응답 구조 변경에 따른 api 및 쿼리 훅 리팩토링

--- src/hooks/mutations/follow/useFollowingMutation.ts
+++ src/hooks/mutations/follow/useFollowingMutation.ts
@@ -1,8 +1,10 @@
+import { userKeys } from '@/hooks/queries/user/userQueryKeys';
 import { followKeys } from '@/hooks/queries/follow/followQueryKeys';
 import api from '@/services/apis/api';
 import { ApiErrorResponse } from '@/services/apis/types/commonApi';
 import { FollowResponse } from '@/services/apis/types/followAPI';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { IOhterUser } from '@/services/apis/types/userAPI';
 
 interface UseFollowMutationProps {
   otherUserName: string;
@@ -16,8 +18,12 @@ export function useFollowingMutation({ otherUserName, otherUserImage }: UseFollo
     mutationFn: (userId: number) => api.follow.postFollow(userId),
     onMutate: async (userId) => {
       await queryClient.cancelQueries({ queryKey: followKeys.userFollowings(userId) });
+      await queryClient.cancelQueries({ queryKey: userKeys.otherUser(userId) });
 
-      const previousFollowers = queryClient.getQueryData(followKeys.userFollowings(userId));
+      const followingPreviousFollowers = queryClient.getQueryData(
+        followKeys.userFollowings(userId)
+      );
+      const otherUserPrevious = queryClient.getQueryData(userKeys.otherUser(userId));
 
       queryClient.setQueryData(followKeys.userFollowings(userId), (oldData: FollowResponse) => {
         return {
@@ -28,16 +34,28 @@ export function useFollowingMutation({ otherUserName, otherUserImage }: UseFollo
           ],
         };
       });
+      queryClient.setQueryData(userKeys.otherUser(userId), (oldData: IOhterUser) => {
+        const currentIsFollowing = oldData.isFollowing;
+        return {
+          ...oldData,
+          isFollowing: !currentIsFollowing,
+        };
+      });
 
-      return { previousFollowers };
+      return { followingPreviousFollowers, otherUserPrevious };
     },
 
     onError(_, userId, context: any) {
-      queryClient.setQueryData(followKeys.userFollowings(userId), context.previousHeroData);
+      queryClient.setQueryData(
+        followKeys.userFollowings(userId),
+        context.followingPreviousFollowers
+      );
+      queryClient.setQueryData(userKeys.otherUser(userId), context.otherUserPrevious);
     },
 
     onSuccess: (_, userId) => {
       queryClient.invalidateQueries({ queryKey: followKeys.userFollowings(userId) });
+      queryClient.invalidateQueries({ queryKey: userKeys.otherUser(userId) });
     },
   });
 }
