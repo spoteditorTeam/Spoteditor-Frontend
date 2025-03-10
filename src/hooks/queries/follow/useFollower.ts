6bb74246928a80c78c5243f358c29348e731e466
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-10 19:27:48 +0900

    feat: 팔로잉 버튼 추가

--- src/hooks/queries/follow/useFollower.ts
+++ src/hooks/queries/follow/useFollower.ts
@@ -3,11 +3,10 @@ import { followKeys } from './followQueryKeys';
 import { FollowQueryParams } from '@/services/apis/types/followAPI';
 import api from '@/services/apis/api';
 
-export default function useFollower({
-  page = 1,
-  size = 10,
-  direction = 'ASC',
-}: Partial<FollowQueryParams> = {}) {
+export default function useFollower(
+  isMe: boolean,
+  { page = 1, size = 10, direction = 'ASC' }: Partial<FollowQueryParams> = {}
+) {
   return useInfiniteQuery({
     queryKey: followKeys.followerList({ page, size, direction }),
     queryFn: ({ pageParam }) => api.follow.getFollower({ ...pageParam, size, direction }),
@@ -16,5 +15,6 @@ export default function useFollower({
       const nextPage = lastPage.pageNumber + 1;
       return nextPage <= lastPage.totalPages ? { page: nextPage } : null;
     },
+    enabled: isMe,
   });
 }
--- src/hooks/queries/user/useOtherUser.ts
+++ src/hooks/queries/user/useOtherUser.ts
@@ -1,11 +1,11 @@
-import { IUser } from '@/services/apis/types/userAPI';
+import { IOhterUser } from '@/services/apis/types/userAPI';
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { userKeys } from './userQueryKeys';
 import api from '@/services/apis/api';
 
 export default function useOtherUser(
   userId: number,
-  queryOptions?: Partial<UseQueryOptions<IUser, Error>>
+  queryOptions?: Partial<UseQueryOptions<IOhterUser, Error>>
 ) {
   return useQuery({
     queryKey: userKeys.otherUser(userId),
