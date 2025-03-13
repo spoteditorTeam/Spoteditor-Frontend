870c4815f649b89e777f5e3a50753b4705ca29af
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-13 22:58:41 +0900

    feat: 상세 페이지에서만 단일 로그 낙관적 업데이트 적용
    - 상세 페이지(`/log/...`)에서만 단일로그에 낙관적 업데이트 적용
    - 단일 로그 데이터의 인터페이스 수정

--- src/hooks/mutations/follow/useFollowingMutation.ts
+++ src/hooks/mutations/follow/useFollowingMutation.ts
@@ -5,6 +5,22 @@ import { ApiErrorResponse } from '@/services/apis/types/commonApi';
 import { FollowResponse } from '@/services/apis/types/followAPI';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { IOhterUser } from '@/services/apis/types/userAPI';
+import { useLocation, useParams } from 'react-router-dom';
+import { logKeys } from '@/hooks/queries/log/logQueryKeys';
+import { Address } from 'cluster';
+import { Image } from '@/services/apis/types/registerAPI.type';
+
+export interface PlaceInLog {
+  placeId: number;
+  userId: number;
+  userName: string;
+  userImage: string;
+  name: string;
+  description: string;
+  address: Address;
+  category: string;
+  images: Image[];
+}
 
 interface UseFollowMutationProps {
   otherUserName: string;
@@ -12,6 +28,12 @@ interface UseFollowMutationProps {
 }
 
 export function useFollowingMutation({ otherUserName, otherUserImage }: UseFollowMutationProps) {
+  const { pathname } = useLocation();
+  const { placeLogId } = useParams();
+
+  /* 상세페이지에서 팔로잉할 경우에만 낙관적 단일로그 낙관적 업데이트 */
+  const detailPage = pathname.startsWith('/log');
+
   const queryClient = useQueryClient();
 
   return useMutation<void, ApiErrorResponse, number>({
@@ -19,11 +41,17 @@ export function useFollowingMutation({ otherUserName, otherUserImage }: UseFollo
     onMutate: async (userId) => {
       await queryClient.cancelQueries({ queryKey: followKeys.userFollowings(userId) });
       await queryClient.cancelQueries({ queryKey: userKeys.otherUser(userId) });
+      if (detailPage && placeLogId) {
+        await queryClient.cancelQueries({ queryKey: logKeys.detail(Number(placeLogId)) });
+      }
 
       const followingPreviousFollowers = queryClient.getQueryData(
         followKeys.userFollowings(userId)
       );
       const otherUserPrevious = queryClient.getQueryData(userKeys.otherUser(userId));
+      const logPrevious = detailPage
+        ? queryClient.getQueryData(logKeys.detail(Number(placeLogId)))
+        : undefined;
 
       queryClient.setQueryData(followKeys.userFollowings(userId), (oldData: FollowResponse) => {
         return {
@@ -41,8 +69,16 @@ export function useFollowingMutation({ otherUserName, otherUserImage }: UseFollo
           isFollowing: !currentIsFollowing,
         };
       });
-
-      return { followingPreviousFollowers, otherUserPrevious };
+      if (detailPage && placeLogId) {
+        queryClient.setQueryData(logKeys.detail(Number(placeLogId)), (oldData: IOhterUser) => {
+          const currentIsFollowing = oldData.isFollowing;
+          return {
+            ...oldData,
+            isFollowing: !currentIsFollowing,
+          };
+        });
+      }
+      return { followingPreviousFollowers, otherUserPrevious, logPrevious };
     },
 
     onError(_, userId, context: any) {
@@ -51,11 +87,17 @@ export function useFollowingMutation({ otherUserName, otherUserImage }: UseFollo
         context.followingPreviousFollowers
       );
       queryClient.setQueryData(userKeys.otherUser(userId), context.otherUserPrevious);
+      if (detailPage && context.logPrevious) {
+        queryClient.setQueryData(logKeys.detail(Number(placeLogId)), context.logPrevious);
+      }
     },
 
     onSuccess: (_, userId) => {
       queryClient.invalidateQueries({ queryKey: followKeys.userFollowings(userId) });
       queryClient.invalidateQueries({ queryKey: userKeys.otherUser(userId) });
+      if (detailPage && placeLogId) {
+        queryClient.invalidateQueries({ queryKey: logKeys.detail(Number(placeLogId)) });
+      }
     },
   });
 }
