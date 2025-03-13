422d954bec4fec7655e371df838e7b7fb00353d2
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-14 03:54:26 +0900

    feat: 프로필 저장 로그가 없을 경우 빈 페이지 추가
     - 프로필 저장 로그 또는 관련 데이터가 없을 경우 대비하여 빈 페이지를 추가

--- src/pages/profile/saved-logs/index.tsx
+++ src/pages/profile/saved-logs/index.tsx
@@ -13,6 +13,7 @@ import useUserBookmarkLogs from '@/hooks/queries/userLog/useUserBookmarkLogs';
 import { getImgFromCloudFront } from '@/utils/getImgFromCloudFront';
 import { Link, useSearchParams } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import NotProfileData from '../NotProfileData';
 
 function SavedLogs() {
   const { user } = useUser();
@@ -39,7 +40,7 @@ function SavedLogs() {
     <>
       {isPending ? (
         <Loading className="min-h-[350px]" />
-      ) : (
+      ) : data?.content.length !== 0 ? (
         <>
           <PostCardWrapper className="mb-[50px]">
             {data?.content.map((log) => (
@@ -63,6 +64,8 @@ function SavedLogs() {
             />
           </section>
         </>
+      ) : (
+        <NotProfileData />
       )}
     </>
   );
