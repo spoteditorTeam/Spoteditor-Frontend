d5f2c467ee84a0ed53e110ecdf6241eeed54cc39
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-14 00:11:46 +0900

    refactor: useUser 훅 staleTime 수정
     - 팔로잉, 팔로워 목록 업데이트를 위해 프로필 페이지에서 새로 데이터 가져오게 수정

--- src/hooks/queries/user/useUser.ts
+++ src/hooks/queries/user/useUser.ts
@@ -1,16 +1,20 @@
 import { authUserApi } from '@/services/apis/userApi';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { userKeys } from '@/hooks/queries/user/userQueryKeys';
 
 type UserState = 'userOnly' | 'nonUserOnly';
 
 export default function useUser(userState?: UserState) {
+  const { pathname } = useLocation();
+
+  const profilePage = pathname.startsWith('/profile');
+
   const { data, error } = useQuery({
     queryKey: userKeys.me(),
     queryFn: () => authUserApi.getUser(),
-    staleTime: Infinity,
+    staleTime: profilePage ? 0 : Infinity,
   });
 
   const nav = useNavigate();
