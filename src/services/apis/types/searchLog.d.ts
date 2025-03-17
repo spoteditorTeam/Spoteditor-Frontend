3bac4e10840c4c86627149d9b9641e1daa6062ef
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-17 19:09:52 +0900

    refactor: api 파라미터 변경에 따른 관련 패칭함수들 수정

--- src/services/apis/types/searchLog.d.ts
+++ src/services/apis/types/searchLog.d.ts
@@ -1,14 +1,14 @@
-import { PageNation } from "./commonApi";
-import { LogContent } from "./logAPI.type";
+import { PageNation } from './commonApi';
+import { LogContent } from './logAPI.type';
 import { LogsQueryParams } from '@/services/apis/types/logAPI.type';
 
- export interface SearchLogsNameQueryParams extends LogsQueryParams {
-    name: string;
- }
+export interface SearchLogsNameQueryParams extends LogsQueryParams {
+  name: string;
+}
 
- export interface SearchLogsAddressQueryParams extends LogsQueryParams {
-    sido: string;
-    sigungu: string;
- }
+export interface SearchLogsAddressQueryParams extends LogsQueryParams {
+  sido: string;
+  bname: string;
+}
 
-export type SearchLogResponse = PageNation<LogContent[]>
+export type SearchLogResponse = PageNation<LogContent[]>;
--- src/store/searchStore.ts
+++ src/store/searchStore.ts
@@ -11,15 +11,28 @@ export const useSearchStore = create<SearchState>((set) => ({
 }));
 
 interface CitySearchState {
+  isDropBox: boolean;
   sido: string;
-  sigungu: string;
+  bname: string;
+  openDropBox: () => void;
+  closeDropBox: () => void;
   setSido: (sido: string) => void;
-  setSigungu: (sigungu: string) => void;
+  setBname: (bname: string) => void;
 }
 
 export const useCitySearchStore = create<CitySearchState>((set) => ({
+  isDropBox: false,
   sido: '',
-  sigungu: '',
-  setSido: () => set((state) => ({ sido: state.sido })),
-  setSigungu: () => set((state) => ({ sigungu: state.sido })),
+  bname: '',
+
+  openDropBox: () => set(() => ({ isDropBox: true })),
+  closeDropBox: () => set(() => ({ isDropBox: false })),
+  setSido: (sido) => set(() => ({ sido })),
+  setBname: (bname) => set(() => ({ bname })),
+  /* setSigungus: () =>
+    set((state) => ({
+      sigungus: state.sigungus.includes(state.sigungu)
+        ? state.sigungus // 이미 존재하면 변경하지 않음
+        : [...state.sigungus, state.sigungu], // 새로운 값 추가
+    })), */
 }));
