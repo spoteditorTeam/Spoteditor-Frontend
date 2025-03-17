2269c230535ee166df640a51b7ad79d621e450b5
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-17 22:15:50 +0900

    fix: 버셀 충돌에러 해결

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -3,10 +3,10 @@ import LogAPI from './logApi';
 import PlaceAPI from './placeApi';
 import RegisterAPI from './registerApi';
 import FollowAPI from './followApi';
-import { OtherUser } from './userApi';
-import UserLog from './userLogApi';
-import SearchLog from './searchLog.Api';
 import NotificationAPI from './notificationApi';
+import UserLogAPI from './userLogApi';
+import { OtherUserAPI } from './userApi';
+import SearchLog from './searchLog.Api';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -27,8 +27,8 @@ class API {
     this.userLog = new UserLogAPI(this.#axios);
     this.place = new PlaceAPI(this.#axios);
     this.follow = new FollowAPI(this.#axios);
-    this.otherUser = new OtherUser(this.#axios);
-    this.searchLog = new SearchLog(this.#axios)
+    this.otherUser = new OtherUserAPI(this.#axios);
+    this.searchLog = new SearchLog(this.#axios);
     this.notification = new NotificationAPI(this.#axios);
   }
 }
