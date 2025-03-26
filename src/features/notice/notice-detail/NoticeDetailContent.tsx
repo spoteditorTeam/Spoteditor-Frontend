936e08557f73ba2d7182380e2c7ded8ebaf41004
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-03-27 03:43:08 +0900

    refacor: 공지사항 상세페이지 헤더 모션 수정
     - CSS 마스킹 효과 적용

--- src/features/notice/notice-detail/NoticeDetailContent.tsx
+++ src/features/notice/notice-detail/NoticeDetailContent.tsx
@@ -4,7 +4,7 @@ interface NoticeDetailContentProps {
 
 function NoticeDetailContent({ content }: NoticeDetailContentProps) {
   return (
-    <section className="flex px-4 py-10 border-t-[1px] border-t-primary-50">
+    <section className="flex px-4 py-10">
       <span className="font-medium leading-[23.8px] text-[14px] text-primary-600 ">{content}</span>
     </section>
   );
