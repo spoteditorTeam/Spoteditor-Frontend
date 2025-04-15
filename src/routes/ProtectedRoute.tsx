d3db063e8c4ef97cb7ecfe23d7a0a9177d8106cd
hayanLee <hayanlee.dev@gmail.com>
2025-04-15 14:08:52 +0900

    fix : ProtectedRoute에 loginModal open 추가

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,11 +1,16 @@
 import { HOME } from '@/constants/pathname';
 import useAuth from '@/hooks/queries/user/useAuth';
+import { useLoginMoalStore } from '@/store/loginStore';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
+  const { openLoginModal } = useLoginMoalStore();
   const { data: user } = useAuth();
 
-  if (!user) return <Navigate to={HOME} replace />;
+  if (!user) {
+    openLoginModal();
+    return <Navigate to={HOME} replace />;
+  }
   return <Outlet />;
 };
 
