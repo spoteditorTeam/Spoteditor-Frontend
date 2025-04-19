e14c4664dbea8ce4a623841f991166f706a855b0
hayanLee <hayanlee.dev@gmail.com>
2025-04-19 23:28:15 +0900

    bugfix : ProtectedRoute 무한루프 경고 해결
    - 수동 스크롤 제어에서 ScrollRestoration 으로 변경
    - ContentLayout, MainLayout 수정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,11 @@
-import ScrollToTop from '@/components/ScrollToTop';
 import ContentLayout from '@/layouts/ContentLayout';
-import { Outlet } from 'react-router-dom';
+import { ScrollRestoration } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col items-center web:min-w-[1440px] w-full h-dvh">
-      <ContentLayout>
-        <ScrollToTop />
-        <div className="w-full grow">
-          <Outlet />
-        </div>
-      </ContentLayout>
+      <ScrollRestoration />
+      <ContentLayout />
     </div>
   );
 };
