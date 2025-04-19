91d7bb318e67a7d0199d6b7c5865e78f4d43be46
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-04-19 20:31:04 +0900

    feat: 구글 로그인 기능 추가

---
+++ src/features/login/SocialLoginButtons/SocialLoginButtons.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import kakaoLoginButton from '@/assets/login/kakao-login-button.png';
+import googleLoginButton from '@/assets/login/google-login-button.png';
+import SocialLoginButton from './SocialLoginButton';
+
+export default function SocialLoginButtons() {
+  const nav = useNavigate();
+
+  const handleLogin = (socialName: string) => {
+    nav('/');
+    const BASE_PATH = window.location.origin;
+    window.location.href = `${
+      import.meta.env.VITE_SOCIAL_LOGIN_URL
+    }/${socialName}?redirect=${BASE_PATH}`;
+  };
+  return (
+    <section className="flex flex-col w-full gap-2.5 py-5">
+      <SocialLoginButton
+        socialName="kakao"
+        socialButtonImg={kakaoLoginButton}
+        handleLogin={handleLogin}
+      />
+      <SocialLoginButton
+        socialName="google"
+        socialButtonImg={googleLoginButton}
+        handleLogin={handleLogin}
+      />
+    </section>
+  );
+}
