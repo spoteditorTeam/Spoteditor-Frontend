6e50db38caef43db061b4056e0c9c50d123d913d
hayanLee <hayanlee.dev@gmail.com>
2025-04-21 01:15:20 +0900

    style : footer ui 수정

--- src/components/Footer/MainFooter.tsx
+++ src/components/Footer/MainFooter.tsx
@@ -1,16 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const MainFooter = () => {
   return (
-    <footer className="w-full bg-black px-50px">
-      <div className="flex gap-4 text-primary-200 border-b border-primary-950 mt-20 web:mt-[200px] py-4 web:py-8">
+    <footer className="w-full bg-black px-4 web:px-[50px] text-text-sm web:text-text-md">
+      <div className="flex gap-4 text-light-200 border-b border-light-950 mt-20 web:mt-[200px] py-4 web:py-8">
         <div className="flex gap-4 web:gap-8 web:grow">
           <a href={'https://tally.so/r/nrYJEo'} target="_blank">
             Contact Us
           </a>
         </div>
-        <p>Instragram</p>
+        <Link to={'#'}>Instragram</Link>
       </div>
 
-      <div className="flex flex-col web:flex-row gap-3 justify-between text-primary-600 mt-4 mb-10">
+      <div className="flex flex-col web:flex-row gap-3 justify-between text-light-600 mt-4 mb-10">
         <p>©2025 Spoteditor. All Rights are reserved️</p>
         <div className="flex gap-6">
           <p>Privacy Policy</p>
