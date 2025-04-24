a4316f601d80ab9836597bd2ed000817674ae1c1
hayanLee <hayanlee.dev@gmail.com>
2025-04-24 23:26:24 +0900

    refactor : 상세페이지 컴포넌트 리팩토링
    - FloatingButton, BannerActionButtons, DetailBanner 생성
    - LogAuthorInfo에 props로 전달받던 userId를 authorId로 이름 변경
    - ui 수정

--- src/features/profile/LogAuthorInfo.tsx
+++ src/features/profile/LogAuthorInfo.tsx
@@ -6,19 +6,19 @@ import { Link } from 'react-router-dom';
 import LogFollowingButton from '../follow/LogFollowingButton';
 
 interface LogAuthorInfoProps {
-  userId: number;
+  authorId: number; // 작성자 아이디
+  isOwner: boolean;
 }
 
-export default function LogAuthorInfo({ userId }: LogAuthorInfoProps) {
+export default function LogAuthorInfo({ authorId, isOwner }: LogAuthorInfoProps) {
   const { data: userData } = useUser();
-  const isMe = userData?.userId === userId;
-  const { data: otherUserData } = useOtherUser(userId, { enabled: !!userId && !isMe });
+  const { data: otherUserData } = useOtherUser(authorId, { enabled: !!authorId && !isOwner });
+  const data = isOwner ? userData : otherUserData;
 
-  const data = isMe ? userData : otherUserData;
   return (
     <div className="flex items-center gap-2 min-h-10">
       <div className="flex items-center gap-2">
-        <Link to={`/profile/${userId}`} className="flex items-center gap-2">
+        <Link to={`/profile/${authorId}`} className="flex items-center gap-2">
           <Avatar className="w-6 h-6">
             <AvatarImage src={data?.profileImage.imageUrl || userDefaultAvatar} alt="user Avatar" />
           </Avatar>
@@ -26,9 +26,9 @@ export default function LogAuthorInfo({ userId }: LogAuthorInfoProps) {
             <p className="font-semibold text-text-sm">{data?.name}</p>
           </div>
         </Link>
-        {userData && userData?.userId && !isMe && (
+        {userData && !isOwner && (
           <LogFollowingButton
-            otherUserId={userId}
+            otherUserId={authorId}
             isFollowing={Boolean(otherUserData?.isFollowing)}
           />
         )}
